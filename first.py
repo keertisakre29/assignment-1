29a0756abb594bf49ae27456850006ebca03be7a
keertisakre29 <127831271+keertisakre29@users.noreply.github.com>
2025-03-06 13:14:58 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,20 @@
+# f = open("sample_text_file.txt","w")
+# f.write("i want to learn javascript Tomorrow")
+# f.close()
+
+# f = open("sample_text_file.txt","a")
+# f.write("then i,ll move to reactJS")
+# f.close()
+
+
+
+
+
+
+
+f = open("sample_text_file.txt","r")
+ 
+f_content = f.read()
+
+
+print(f_content)
